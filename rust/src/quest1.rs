e50787944e0c9d3bf51e44cac5dcb18ca868ed39
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-19 16:41:39 +0100

    quest 1 part 1

--- rust/src/quest1.rs
+++ rust/src/quest1.rs
@@ -3,7 +3,25 @@ use crate::{Quest, QuestResult};
 pub const PARTS: Quest = [part1, part2, part3];
 
 fn part1(input: String) -> QuestResult {
-    todo!("\n{input}")
+    let (names, moves) = input.split_once("\n\n").unwrap();
+
+    let names: Vec<_> = names.split(',').collect();
+
+    let mut i: usize = 0;
+
+    for m in moves.split(',') {
+        let (dir, n) = m.split_at(1);
+
+        let n: usize = n.parse().unwrap();
+
+        match dir {
+            "R" => i = (i + n).min(names.len() - 1),
+            "L" => i = i.saturating_sub(n),
+            _ => panic!(),
+        }
+    }
+
+    QuestResult::Text(names[i].to_owned())
 }
 
 fn part2(input: String) -> QuestResult {
