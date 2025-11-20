8dfd8a703d599cd3147cf37f7cdbe20f08fea9f2
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-20 16:22:59 +0100

    quest 5

--- rust/src/quest5.rs
+++ rust/src/quest5.rs
@@ -1,16 +1,18 @@
 use itertools::{Itertools, MinMaxResult};
+use rayon::str::ParallelString;
 
 use crate::{Quest, QuestResult};
 
 pub const PARTS: Quest = [part1, part2, part3];
 
-fn compute_quality(line: &str) -> u64 {
-    let mut numbers = line
-        .split_once(':')
-        .unwrap()
-        .1
-        .split(',')
-        .map(|x| x.parse::<u8>().unwrap());
+type Fishbone = Vec<(u8, Option<u8>, Option<u8>)>;
+
+fn construct_fishbone(line: &str) -> (usize, Fishbone) {
+    let (id, numbers) = line.split_once(':').unwrap();
+
+    let id = id.parse().unwrap();
+
+    let mut numbers = numbers.split(',').map(|x| x.parse::<u8>().unwrap());
 
     let mut fishbone = vec![(numbers.next().unwrap(), None, None)];
 
@@ -33,9 +35,17 @@ fn compute_quality(line: &str) -> u64 {
         }
     }
 
-    fishbone
-        .into_iter()
-        .fold(0, |x, (d, _, _)| 10 * x + d as u64)
+    (id, fishbone)
+}
+
+fn digits_to_num<I: Iterator<Item = u8>>(it: I) -> u64 {
+    it.fold(0, |x, d| 10 * x + d as u64)
+}
+
+fn compute_quality(line: &str) -> u64 {
+    let (_, fishbone) = construct_fishbone(line);
+
+    digits_to_num(fishbone.into_iter().map(|(d, _, _)| d))
 }
 
 fn part1(input: String) -> QuestResult {
@@ -52,6 +62,33 @@ fn part2(input: String) -> QuestResult {
     QuestResult::Number(ans as i64)
 }
 
+fn compute_order_parameters(id: usize, fishbone: Fishbone) -> Vec<u64> {
+    let mut params = vec![digits_to_num(fishbone.iter().map(|&(d, _, _)| d))];
+
+    params.extend(fishbone.into_iter().map(|(spine, left, right)| {
+        digits_to_num([left, Some(spine), right].into_iter().flatten())
+    }));
+
+    params.push(id as u64);
+
+    params
+}
+
 fn part3(input: String) -> QuestResult {
-    todo!("\n{input}")
+    let mut params: Vec<_> = input
+        .lines()
+        .map(construct_fishbone)
+        .map(|(id, fishbone)| compute_order_parameters(id, fishbone))
+        .collect();
+
+    params.sort();
+
+    let ans: u64 = params
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(i, params)| (i + 1) as u64 * params.last().unwrap())
+        .sum();
+
+    QuestResult::Number(ans as i64)
 }
