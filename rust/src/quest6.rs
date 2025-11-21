be0cad6f16d3d00ea2d5471838fb1bd0ea47a16d
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-21 12:36:27 +0100

    quest 6 fast

--- rust/src/quest6.rs
+++ rust/src/quest6.rs
@@ -43,36 +43,75 @@ fn part3(input: String) -> QuestResult {
     let mut knights = [0; 3];
     let mut novices = [0; 3];
 
-    let mut pairs = 0;
+    let mut init_pairs = 0;
 
     let input = input.as_bytes();
 
-    let totlen = input.len() * REPEATS;
+    let n_passes_to_fill_window = MAXDIST.div_ceil(input.len());
+    assert!(n_passes_to_fill_window <= REPEATS);
 
-    for head in 0..totlen {
-        if head > MAXDIST {
-            let tail = head - MAXDIST - 1;
-            let x_tail = input[tail % input.len()];
-            let i = ((x_tail & !0x20) - b'A') as usize;
+    let mut head = 0;
 
-            if (x_tail & 0x20) == 0 {
-                knights[i] -= 1;
-            } else {
-                novices[i] -= 1;
+    for _ in 0..n_passes_to_fill_window {
+        for x_head in input {
+            if head > MAXDIST {
+                let tail = head - MAXDIST - 1;
+                let x_tail = input[tail];
+                let i = ((x_tail & !0x20) - b'A') as usize;
+
+                if (x_tail & 0x20) == 0 {
+                    knights[i] -= 1;
+                } else {
+                    novices[i] -= 1;
+                }
             }
-        }
 
-        let x_head = input[head % input.len()];
-        let i = ((x_head & !0x20) - b'A') as usize;
+            let i = ((x_head & !0x20) - b'A') as usize;
 
-        if (x_head & 0x20) == 0 {
-            pairs += novices[i];
-            knights[i] += 1;
-        } else {
-            pairs += knights[i];
-            novices[i] += 1;
+            if (x_head & 0x20) == 0 {
+                init_pairs += novices[i];
+                knights[i] += 1;
+            } else {
+                init_pairs += knights[i];
+                novices[i] += 1;
+            }
+
+            head += 1;
         }
     }
 
-    QuestResult::Number(pairs)
+    let mut tail = head - MAXDIST - 1;
+    assert!(tail < input.len());
+
+    let mut bulk_pairs = 0;
+
+    for x_head in input {
+        let x_tail = input[tail];
+        let i = ((x_tail & !0x20) - b'A') as usize;
+
+        if (x_tail & 0x20) == 0 {
+            knights[i] -= 1;
+        } else {
+            novices[i] -= 1;
+        }
+
+        let i = ((x_head & !0x20) - b'A') as usize;
+
+        if (x_head & 0x20) == 0 {
+            bulk_pairs += novices[i];
+            knights[i] += 1;
+        } else {
+            bulk_pairs += knights[i];
+            novices[i] += 1;
+        }
+
+        tail += 1;
+        if tail == input.len() {
+            tail = 0;
+        }
+    }
+
+    let pairs = init_pairs + bulk_pairs * (REPEATS - n_passes_to_fill_window);
+
+    QuestResult::Number(pairs as i64)
 }
