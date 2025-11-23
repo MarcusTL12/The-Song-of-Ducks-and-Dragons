a13f628d0fc89088e7db0dac69e1bb7b2fce4684
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-23 18:10:51 +0100

    part 2

--- rust/src/quest10.rs
+++ rust/src/quest10.rs
@@ -38,9 +38,10 @@ fn recurse(
     sheep
 }
 
-fn part1(mut input: String) -> QuestResult {
-    input.push('\n');
-    let mut grid = input_to_grid_mut(unsafe { input.as_bytes_mut() });
+fn part1(input: String) -> QuestResult {
+    let mut input = input.into_bytes();
+    input.push(b'\n');
+    let mut grid = input_to_grid_mut(&mut input);
 
     let ((i, j), _) = grid.indexed_iter().find(|&(_, &x)| x == b'D').unwrap();
 
@@ -49,8 +50,124 @@ fn part1(mut input: String) -> QuestResult {
     QuestResult::Number(sheep as i64)
 }
 
+fn propagate_dragon(grid: &mut ArrayViewMut2<u8>) -> usize {
+    let (h, w) = grid.dim();
+
+    for i in 0..h {
+        for j in 0..w {
+            if (grid[[i, j]] & 0b0000_0001) == 0 {
+                continue;
+            }
+
+            grid[[i, j]] &= 0b1111_1110;
+
+            for [nx, ny] in [[1, 2], [2, 1]] {
+                for sx in [-1, 1] {
+                    let dx = nx * sx;
+                    let x = (j as isize + dx) as usize;
+
+                    for sy in [-1, 1] {
+                        let dy = ny * sy;
+                        let y = (i as isize + dy) as usize;
+
+                        if let Some(tile) = grid.get_mut([y, x]) {
+                            *tile |= 0b0000_1000;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut eaten = 0;
+
+    for x in grid.iter_mut() {
+        if (*x & 0b0000_1000) != 0 {
+            if (*x & 0b0000_0110) == 0b0000_0010 {
+                eaten += 1;
+                *x = (*x & 0b1111_0101) | 0b0000_0001;
+            } else {
+                *x = (*x & 0b1111_0111) | 0b0000_0001;
+            }
+        }
+    }
+
+    eaten
+}
+
+fn propagate_sheep(grid: &mut ArrayViewMut2<u8>) -> usize {
+    let (h, w) = grid.dim();
+
+    let mut eaten = 0;
+
+    for i in (1..h).rev() {
+        for j in 0..w {
+            let mut tile = grid[[i, j]];
+            if (grid[[i - 1, j]] & 0b0000_0010) != 0 {
+                tile |= 0b0000_0010;
+                if (tile & 0b0000_0101) == 0b0000_0001 {
+                    eaten += 1;
+                    tile &= 0b1111_1101;
+                }
+            } else {
+                tile &= 0b1111_1101;
+            }
+
+            grid[[i, j]] = tile;
+        }
+    }
+
+    for j in 0..w {
+        grid[[0, j]] &= 0b1111_1101;
+    }
+
+    eaten
+}
+
 fn part2(input: String) -> QuestResult {
-    todo!("\n{input}")
+    let mut input = input.into_bytes();
+    input.push(b'\n');
+
+    for x in &mut input {
+        *x = match *x {
+            b'.' => 0b0000_0000,
+            b'D' => 0b0000_0001,
+            b'S' => 0b0000_0010,
+            b'#' => 0b0000_0100,
+            _ => *x,
+        }
+    }
+
+    let mut grid = input_to_grid_mut(&mut input);
+
+    let mut eaten = 0;
+
+    for _ in 0..20 {
+        eaten += propagate_dragon(&mut grid);
+        eaten += propagate_sheep(&mut grid);
+    }
+
+    // for r in grid.rows() {
+    //     for x in r {
+    //         print!(
+    //             "{}",
+    //             match x {
+    //                 0b0000_0000 => '.',
+    //                 0b0000_0001 => 'D',
+    //                 0b0000_0010 => 'S',
+    //                 0b0000_0011 => 'X',
+    //                 0b0000_0100 => '#',
+    //                 0b0000_0101 => 'd',
+    //                 0b0000_0110 => 's',
+    //                 0b0000_0111 => '+',
+    //                 _ => '!',
+    //             }
+    //         )
+    //     }
+    //     println!();
+    // }
+
+    QuestResult::Number(eaten as i64)
 }
 
 fn part3(input: String) -> QuestResult {
