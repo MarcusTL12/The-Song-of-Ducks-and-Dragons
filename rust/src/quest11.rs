c9d54c8a355e03ae5eff45f87d4b28a0cff5393e
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-25 15:53:10 +0100

    part 2

--- rust/src/quest11.rs
+++ rust/src/quest11.rs
@@ -44,11 +44,44 @@ fn part1(input: String) -> QuestResult {
         .map(|(i, x)| (i as u32 + 1) * x)
         .sum::<u32>();
 
-    QuestResult::Number(ans as i64) 
+    QuestResult::Number(ans as i64)
 }
 
 fn part2(input: String) -> QuestResult {
-    todo!("\n{input}")
+    let mut columns: Vec<u32> =
+        input.lines().map(|l| l.parse().unwrap()).collect();
+
+    let mut dir = false;
+
+    let mut r = 0;
+
+    loop {
+        let mut did_round = false;
+
+        for i in 1..columns.len() {
+            let [a, b] = columns.get_disjoint_mut([i - 1, i]).unwrap();
+
+            if !dir && *a > *b {
+                *a -= 1;
+                *b += 1;
+                did_round = true;
+            } else if dir && *a < *b {
+                *a += 1;
+                *b -= 1;
+                did_round = true;
+            }
+        }
+
+        if did_round {
+            r += 1;
+        } else if !dir {
+            dir = true;
+        } else {
+            break;
+        }
+    }
+
+    QuestResult::Number(r as i64)
 }
 
 fn part3(input: String) -> QuestResult {
