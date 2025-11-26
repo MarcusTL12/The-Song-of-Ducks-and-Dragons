8cb5b35d7b0aefbccad73acc395c13a18b498abb
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-11-26 17:13:29 +0100

    quest 14 brute force

--- rust/src/quest14.rs
+++ rust/src/quest14.rs
@@ -1,4 +1,5 @@
-use ndarray::{Array2, ArrayView2, ArrayViewMut2};
+use bitarray::BitArray;
+use ndarray::ArrayViewMut2;
 
 use crate::{Quest, QuestResult, util::input_to_grid_mut};
 
@@ -84,33 +85,109 @@ fn part2(mut input: String) -> QuestResult {
     QuestResult::Number(ans as i64)
 }
 
-fn look_for_pattern(grid: ArrayView2<u8>, pattern: ArrayView2<u8>) -> bool {
-    pattern
-        .indexed_iter()
-        .all(|((k, l), &x)| grid[[13 + k, 13 + l]] == x)
+// fn look_for_pattern(grid: ArrayView2<u8>, pattern: ArrayView2<u8>) -> bool {
+//     pattern.indexed_iter().all(|((k, l), &x)| {
+//         *unsafe { grid.get([13 + k, 13 + l]).unwrap_unchecked() } == x
+//     })
+// }
+
+const W: usize = 34;
+const N: usize = (W * W).div_ceil(usize::BITS as usize);
+
+const RIGHT_PAD: BitArray<N> = BitArray([
+    0xfffffffbfffffffe,
+    0xffffffbfffffffef,
+    0xfffffbfffffffeff,
+    0xffffbfffffffefff,
+    0xfffbfffffffeffff,
+    0xffbfffffffefffff,
+    0xfbfffffffeffffff,
+    0xbfffffffefffffff,
+    0xfffffffeffffffff,
+    0xffffffeffffffffb,
+    0xfffffeffffffffbf,
+    0xffffeffffffffbff,
+    0xfffeffffffffbfff,
+    0xffeffffffffbffff,
+    0xfeffffffffbfffff,
+    0xeffffffffbffffff,
+    0xffffffffbfffffff,
+    0xfffffffbfffffffe,
+    0xf,
+]);
+
+const LEFT_PAD: BitArray<N> = BitArray([
+    0xfffffffdffffffff,
+    0xffffffdffffffff7,
+    0xfffffdffffffff7f,
+    0xffffdffffffff7ff,
+    0xfffdffffffff7fff,
+    0xffdffffffff7ffff,
+    0xfdffffffff7fffff,
+    0xdffffffff7ffffff,
+    0xffffffff7fffffff,
+    0xfffffff7fffffffd,
+    0xffffff7fffffffdf,
+    0xfffff7fffffffdff,
+    0xffff7fffffffdfff,
+    0xfff7fffffffdffff,
+    0xff7fffffffdfffff,
+    0xf7fffffffdffffff,
+    0x7fffffffdfffffff,
+    0xfffffffdffffffff,
+    0x7,
+]);
+
+const GRID_PAD: BitArray<N> = BitArray([
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xffffffffffffffff,
+    0xf,
+]);
+
+fn do_round_static(grid: BitArray<N>) -> BitArray<N> {
+    let diag1 = (grid << (W + 1) as u32) & RIGHT_PAD;
+    let diag2 = (grid << (W - 1) as u32) & LEFT_PAD;
+    let diag3 = (grid >> (W - 1) as u32) & RIGHT_PAD;
+    let diag4 = (grid >> (W + 1) as u32) & LEFT_PAD;
+
+    (!grid ^ diag1 ^ diag2 ^ diag3 ^ diag4) & GRID_PAD
 }
 
-fn part3(mut input: String) -> QuestResult {
-    let input = unsafe { input.as_mut_vec() };
-    input.push(b'\n');
-    let mut pattern = input_to_grid_mut(input);
-    for x in pattern.iter_mut() {
-        *x = match *x {
-            b'.' => 0,
-            b'#' => 1,
-            _ => panic!(),
+fn part3(input: String) -> QuestResult {
+    let mut grid = BitArray::<N>::new();
+    let mut pattern = BitArray::<N>::new();
+    let mut stencil = BitArray::<N>::new();
+
+    for (i, l) in input.lines().enumerate() {
+        for (j, x) in l.chars().enumerate() {
+            pattern.set((i + 13) * W + j + 13, x == '#');
+            stencil.set((i + 13) * W + j + 13, true);
         }
     }
 
-    let mut grid = Array2::from_elem([34, 34], 0u8);
-
     let mut ans = 0;
 
-    for _ in 0..1_000_000 {
-        do_round(grid.view_mut());
+    for _ in 0..1_000_000_000 {
+        grid = do_round_static(grid);
 
-        if look_for_pattern(grid.view(), pattern.view()) {
-            ans += grid.iter().map(|&x| x as usize).sum::<usize>()
+        if (grid & stencil) == pattern {
+            ans += grid.count_ones();
         }
     }
 
